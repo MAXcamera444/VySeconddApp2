8393b28a9a94aa94639bf424267d2b3891c57a01
maximus <maxcamer@gmail.com>
2018-04-07 17:35:50 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VySeconddApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string str = "1000";
+			short x = Convert.ToInt16(str);			
+			Console.WriteLine(x);
+			Console.ReadKey();  
+        }
+    }
+}
+
+
